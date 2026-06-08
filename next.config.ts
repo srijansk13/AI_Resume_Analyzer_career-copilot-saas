768275974f8bd282454bca239e32d6a45177b905
srijansk13 <srijansk1304@gmail.com>
2026-06-09 03:47:46 +0530

    fix vercel chromium external package

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 const nextConfig: NextConfig = {
   // @ts-ignore - Setting required to allow local network testing on mobile devices
   allowedDevOrigins: ['192.168.31.101'],
+  serverExternalPackages: ['@sparticuz/chromium'],
 };
 
 export default nextConfig;
